69b056c7c43605a5150c8953b7c33f644bbaae89
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-04-03 19:12:57 -0500

    added sidebar and other CSS components

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,35 @@
 //business logic
-var pingPong = function(number) {
-var userNumberArray = [];
- for (var index = 1; index <= number; index += 1) {
-   if ((index % 3 === 0) && (index % 15 === 0)) {
-     userNumberArray.push("pingpong");
-   } else if (index % 3 === 0) {
-     userNumberArray.push("ping");
-   } else if (index % 5 === 0) {
-     userNumberArray.push("pong");
-   } else {
-     userNumberArray.push(index);
-   };
- };
- console.log(userNumberArray);
-	return userNumberArray;
-};
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
+  this.price = 0;
+}
+Pizza.prototype.fullPizza = function() {
+  return this.topping + " " + this.size + " " + this.price;
+}
+var small = { name: "Small", price: 10.00};
+var medium = { name: "Medium", price: 12.00};
+var large = { name: "Large", price: 14.00};
+var pepperoni = { name: "Pepperoni", price: 0};
+var salami = { name: "Salami", price: 0};
+var artichoke = { name: "Artichoke", price: 0};
+var hawaiian = { name: "hawaiian", price: 0};
+var veggie = { name: "veggie", price: 0};
+var anchovy = { name: "Anchovy", price: 1.00};
+var sausage = { name: "Sausage", price: 1.00};
+var chicken = { name: "Chicken", price: 1.00};
 
-// user interface logic
-$(document).ready(function() {
-  $("#ping-pong").submit(function(event) {
+Cost.prototype.fullCost = function () {
+  console.log(this.topping+this.size)
+  return this.topping + " " + this.size;
+}
+
+//user interface logic
+$(document).ready(function(){
+  $("form#order").submit(function(event){
     event.preventDefault();
-    var number = $("input#number").val();
-	  var result = pingPong(number);
-    $("#result").text(result);
+    var inputtedTopping = $("input#topping").val();
+    var inputtedSize = $("input#size").val();
+    var newPizza = new Pizza(inputtedTopping, inputtedSize);
   });
 });
